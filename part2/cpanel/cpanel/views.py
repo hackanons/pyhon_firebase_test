565bd9603b8c134756aa8333a85e99763987f89f
hackanons <37089154+hackanons@users.noreply.github.com>
2018-04-28 14:13:07 +0530

    Add files via upload

---
+++ part2/cpanel/cpanel/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+import pyrebase
+from django.contrib import auth
+config = {
+
+'apiKey': "AIzaSyB0Il0NLQPxxDyMgoE0fOMd4pYUkbkZVvI",
+    'authDomain': "cpanel-5e873.firebaseapp.com",
+    'databaseURL': "https://cpanel-5e873.firebaseio.com",
+    'projectId': "cpanel-5e873",
+    'storageBucket': "cpanel-5e873.appspot.com",
+    'messagingSenderId': "579985583952"
+  }
+
+firebase = pyrebase.initialize_app(config)
+
+authe = firebase.auth()
+
+def signIn(request):
+
+    return render(request, "signIn.html")
+
+def postsign(request):
+    email=request.POST.get('email')
+    passw = request.POST.get("pass")
+    try:
+        user = authe.sign_in_with_email_and_password(email,passw)
+    except:
+        message="invalid credentials"
+        return render(request,"signIn.html",{"messg":message})
+    print(user['idToken'])
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request, "welcome.html",{"e":email})
+def logout(request):
+    auth.logout(request)
+    return render(request,'signIn.html')
+
+
